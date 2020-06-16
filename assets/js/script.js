f0e9625387dc08519ff79c25fd0ed08581e769da
Chris <chris.reviresco@gmail.com>
2020-06-16 20:35:22 +0200

    split js functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,38 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function (event) {
+var taskFormHandler = function (event) {
   event.preventDefault();
 
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-  // create list item
+  // package up data as an object
+  var taskDataObj = {
+    name: taskNameInput,
+    type: taskTypeInput,
+  };
+
+  createTaskEl(taskDataObj);
+};
+
+var createTaskEl = function (taskDataObj) {
   var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
 
   // create div to hold task info annd add to list item
   var taskInfoEl = document.createElement("div");
-  // give it a class name
   taskInfoEl.className = "task-info";
-  //add HTML content to div
   taskInfoEl.innerHTML =
     "<h3 class='task-name'>" +
-    taskNameInput +
+    taskDataObj.name +
     "</h3><span class='task-type'>" +
-    taskTypeInput +
+    taskDataObj.type +
     "</span>";
 
-  listItemEl.className = "task-item";
   listItemEl.appendChild(taskInfoEl);
-
   // add entire list item to list
   tasksToDoEl.appendChild(listItemEl);
 };
 
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
